556da0d74dab760add7dc5c05a92261596053699
fidschau <chaukefide@gmail.com>
2016-09-09 10:39:34 +0200

    Commit 1

---
+++ app/src/main/java/com/gnac/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gnac.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
